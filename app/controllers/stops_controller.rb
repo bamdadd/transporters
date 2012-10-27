ea3b01fcf2671467d1efcdd23e6cda935430411c
Jatin Naik <jsnaik@gmail.com>
2012-10-27 22:42:54 +0100

    find by name api

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -6,4 +6,8 @@ class StopsController < ApplicationController
   def find
     render :json => Stop.find(params[:lat].to_f, params[:long].to_f), :layout => false
   end
+
+  def by_name
+    render :json => Stop.find_by_name(params[:name]), :layout => false
+  end
 end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -18,20 +18,36 @@ class Stop
   end
 
   def self.find(lat,long)
-
     stops = []
     CSV.foreach(File.dirname(__FILE__) + '/MancStops.csv', :headers => true) do |row|
       latitude = row["Latitude"].to_f
       longitude = row["Longitude"].to_f
 
-      if(lat > latitude - 0.5 && lat < latitude + 0.5 &&
-          long > longitude - 0.5 && long < longitude + 0.5 )
+      if(lat > latitude - 0.005 && lat < latitude + 0.005 &&
+          long > longitude - 0.005 && long < longitude + 0.005 )
 
         stop = Stop.new
         stop.common_name = row["CommonName"]
         stop.landmark = row["Landmark"]
         stop.longitude = longitude
         stop.latitude = latitude
+        stop.street = row["Street"]
+        stops << stop
+      end
+    end
+    stops
+  end
+
+  def self.find_by_name(name)
+    stops = []
+    CSV.foreach(File.dirname(__FILE__) + '/MancStops.csv', :headers => true) do |row|
+      if(row["CommonName"].downcase.include? name.downcase)
+
+        stop = Stop.new
+        stop.common_name = row["CommonName"]
+        stop.landmark = row["Landmark"]
+        stop.longitude = row["Longitude"].to_f
+        stop.latitude = row["Latitude"].to_f
         stop.street = row["Street"]
         stops << stop
       end
