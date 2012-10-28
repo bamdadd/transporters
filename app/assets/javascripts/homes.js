c23192dd2da6f47a37eec037180cf3e9f8a536cf
James Linnegar <jlinnega@thoughtworks.com>
2012-10-28 11:15:25 +0000

    fixed auto suggest bus numbers, but broken local stops

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,53 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$( '#find-route').live( 'pageshow', function(event){
-
-      // $("#searchStops").change(getStops);
-      var getStops = function(){
-        var search = $("#searchStops").val();
-        
-        if(search){
-		    $.ajax({
-			     url: '/stops/find/'+search,
-			     dataType: 'json',
-			     success: loadStopList
-			 });
-		} else {
-			clearList();
-		}
-      };
-      var clearList = function(){
-      	$("#stopList").empty();      
-      };
-      var loadStopList = function(data){
-      
-	  	var list = $("#stopList");
-	  	list.empty();
-	  	$.each(data, function(index, value) { 
-			list.append("<li>"+value.common_name +"</li>");
-		});
-	  	list.listview('refresh');
-      };
-
-      var typingTimer;                //timer identifier
-      var doneTypingInterval = 1;  //time in ms, 5 second for example
-      
-      //on keyup, start the countdown
-      $('#searchStops').keyup(function(){
-				  typingTimer = setTimeout(getStops, doneTypingInterval);
-			      });
-      
-      //on keydown, clear the countdown 
-      $('#searchStops').keydown(function(){
-				    clearTimeout(typingTimer);
-				});
- 	  $('.ui-input-clear').live('click', clearList);
-      
-});
 
 
 
-$(function(){
+$( '#find-bus').live( 'pageshow', function(event){
       var getRoute = function(){
         var search = $("#searchRoute").val();
         
@@ -75,7 +31,7 @@ $(function(){
       };
       // $("#searchRoute").change(getRoute);
       var typingTimer;                //timer identifier
-      var doneTypingInterval = 500;  //time in ms, 5 second for example
+      var doneTypingInterval = 5;  //time in ms, 5 second for example
 
 
       
