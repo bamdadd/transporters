8d099c25164df07e7ae12d612b1f731db4fa20dc
Bamdad Dashtban <bamdadd@gmail.com>
2012-10-28 06:29:05 +0000

    @test rexml

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
 gem 'geocoder'
+gem 'geokit'
+gem 'curb'
+gem 'rexml'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
---
+++ app/models/stop_time.rb
@@ -0,0 +1,41 @@
+require "cgi"
+require "uri"
+require "net/http"
+require "rexml"
+
+class StopTime
+  def initialize
+    @traveline_api = "http://nextbus.mxdata.co.uk/nextbuses/1.0/1"
+    #TravelineAPI132:TheiPh1k@
+  end
+
+  def get_times_by_stop_code(stop_code)
+    req_data = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+              <Siri version="1.0" xmlns="http://www.siri.org.uk/"><ServiceRequest>
+            <RequestorRef>TravelineAPI2</RequestorRef>
+            <StopMonitoringRequest version="1.0">
+              <MessageIdentifier>'+stop_code+'</MessageIdentifier>
+              <MonitoringRef>'+stop_code+'</MonitoringRef>
+            </StopMonitoringRequest></ServiceRequest></Siri>'
+
+
+    url = URI.parse(@traveline_api)
+    begin
+      http = Net::HTTP.new(url.host, url.port)
+      request = Net::HTTP::Post.new(url.path)
+      request.body = req_data
+      request.basic_auth 'TravelineAPI132', 'TheiPh1k@'
+      request["Content-Type"] ="application/xml"
+      response = http.request(request)
+      response.code
+      response.body
+    rescue
+      p "Error #{$!}"
+    end
+
+  end
+end
+
+
+t = StopTime.new
+p t.get_times_by_stop_code("1800EB00871")
